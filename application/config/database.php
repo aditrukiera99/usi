e6f6492b2caac44b57a5478c99c63a6e193746d1
MSI\IRWAN <fauzancahya16@gmail.com>
2018-04-24 11:31:21 +0700

    cek

--- application/config/database.php
+++ application/config/database.php
@@ -44,7 +44,7 @@ $active_record = TRUE;
 $db['default']['hostname'] = '118.98.64.43';
 $db['default']['username'] = 'fauzan';
 $db['default']['password'] = 'kalenderhokben';
-$db['default']['database'] = 'usi';
+$db['default']['database'] = 'usi'; 
 
 // $db['default']['hostname'] = 'localhost';
 // $db['default']['username'] = 'root';
